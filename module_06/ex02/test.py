5cf4e083076762882e1fa458584aa89672c348dc
Luuk Van Geffen <lvan-gef@f0r5s19.codam.nl>
2024-10-22 18:06:05 +0200

    removed a test file that we dont need

--- module_06/ex02/test.py
+++
@@ -1,9 +0,0 @@
-
-
-import subprocess
-
-for index in range(10000000000):
-    result = subprocess.run('./identify', shell=True, capture_output=True)
-    if result.returncode != 0:
-        print('error!!!!', index)
-        exit(100)
