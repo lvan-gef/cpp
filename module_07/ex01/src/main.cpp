d2114708af3bf992eac47d6cf02f8e9a4b9b3ea0
Luuk <lvan-gef@student.codam.nl>
2024-10-24 21:14:26 +0200

    Update main.cpp
    Switched back to the simple test for normal build

--- module_07/ex01/src/main.cpp
+++ module_07/ex01/src/main.cpp
@@ -10,40 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../tester/include/tester.hpp"
 #include "../include/iter.hpp"
 
-// void ft_tolower(char &arr) {
-//     arr = std::tolower(static_cast<unsigned char>(arr));
-// }
-//
-// void ft_toupper(char &arr) {
-//     arr = std::toupper(static_cast<unsigned char>(arr));
-// }
-//
-// void simple_test() {
-//     char a[] = {'A', 'B', 'C'};
-//
-//     std::cout << "Original:" << "\n\ta[0]: " << a[0] << "\n\ta[1]: " << a[1]
-//         << "\n\ta[2]: " << a[2] << std::endl
-//         << std::endl;
-//
-//     iter(a, 3, ft_tolower);
-//
-//     std::cout << "Changed:" << "\n\ta[0]: " << a[0] << "\n\ta[1]: " << a[1]
-//         << "\n\ta[2]: " << a[2] << std::endl
-//         << std::endl;
-//
-//     iter(a, 3, ft_toupper);
-//     std::cout << "Changed:" << "\n\ta[0]: " << a[0] << "\n\ta[1]: " << a[1]
-//         << "\n\ta[2]: " << a[2] << std::endl
-//         << std::endl;
-// }
+void ft_tolower(char &arr) {
+    arr = std::tolower(static_cast<unsigned char>(arr));
+}
+
+void ft_toupper(char &arr) {
+    arr = std::toupper(static_cast<unsigned char>(arr));
+}
+
+void simple_test() {
+    char a[] = {'A', 'B', 'C'};
+
+    std::cout << "Original:" << "\n\ta[0]: " << a[0] << "\n\ta[1]: " << a[1]
+        << "\n\ta[2]: " << a[2] << std::endl
+        << std::endl;
+
+    iter(a, 3, ft_tolower);
+
+    std::cout << "Changed:" << "\n\ta[0]: " << a[0] << "\n\ta[1]: " << a[1]
+        << "\n\ta[2]: " << a[2] << std::endl
+        << std::endl;
+
+    iter(a, 3, ft_toupper);
+    std::cout << "Changed:" << "\n\ta[0]: " << a[0] << "\n\ta[1]: " << a[1]
+        << "\n\ta[2]: " << a[2] << std::endl
+        << std::endl;
+}
 
 
 int main() {
-    // simple_test();
-
-    testIter();
+    simple_test();
+    
     return 0;
 }
