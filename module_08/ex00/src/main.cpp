b5a47e2cebce0a2f73d3090fa8ecf873068166ce
luuk <lvan-gef@student.codam.nl>
2024-11-02 01:50:54 +0100

    Update tester

--- module_08/ex00/src/main.cpp
+++ module_08/ex00/src/main.cpp
@@ -1,24 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
-/*                                                        ::::::::            */
+/*   main.cpp                                           :+:    :+:            */
 /*   main.cpp                                           :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: lvan-gef <lvan-gef@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
-/*   Created: 2024/10/28 17:38:27 by lvan-gef      #+#    #+#                 */
-/*   Updated: 2024/11/02 01:34:49 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2024/11/02 01:48:39 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2024/11/02 01:50:25 by lvan-gef      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/easyfind.hpp"
 
-void simple_test() {
+static void simple_test() {
+    std::cout << "Start simple easyfind test" << '\n';
+
     std::list<int> testList = {1, 500, 28, 33, 21, 434, 235, 787, 123432};
 
     try {
         easyfind(testList, 787);
     } catch (std::invalid_argument &e) {
         assert(false && "Target should be find, so something whent wrong\n");
+    } catch (...) {
+        assert(false && "Unknow error should not be here\n");
+    }
+
+    try {
+        easyfind(testList, 0);
+        assert(false && "Should throw a error, so something whent wrong\n");
+    } catch (std::invalid_argument &e) {
+        static_assert(true, "Succesfull throw a error ");
     } catch (...) {
         assert(false && "Unknow error should not be here\n");
     }
@@ -36,6 +47,17 @@ void simple_test() {
     } catch (...) {
         assert(false && "Unknow error should not be here\n");
     }
+
+    try {
+        easyfind(testList, 100000);
+        assert(false && "Should throw a error, so something whent wrong\n");
+    } catch (std::invalid_argument &e) {
+        static_assert(true, "Succesfull throw a error ");
+    } catch (...) {
+        assert(false && "Unknow error should not be here\n");
+    }
+
+    std::cout << "End simple easyfind test" << '\n' << '\n';
 }
 
 int main() {
