0830e1b3f75745576d724b2b14bfca39e8f5edae
luuk <lvan-gef@student.codam.nl>
2024-11-02 02:32:17 +0100

    Works better with tester now

--- module_08/ex01/src/main.cpp
+++ module_08/ex01/src/main.cpp
@@ -1,3 +1,7 @@
+#include "../tester/include/tester.hpp"
+
+
+
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
@@ -6,7 +10,7 @@
 /*   By: lvan-gef <lvan-gef@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/29 18:44:22 by lvan-gef      #+#    #+#                 */
-/*   Updated: 2024/10/29 18:45:45 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2024/11/02 02:12:39 by lvan-gef      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,31 +33,63 @@ static void check_fail(Span sp) {
     }
 }
 
-static void subject_test() {
+static void subject_test(bool isPrint) {
+    if (isPrint == true) {
+    std::cout << "Start subject test" << '\n';
+    }
+
     Span sp = Span(6);
     check_fail(sp);
+
     sp.addNumber(6);
     check_fail(sp);
+
     sp.addNumber(3);
     check_fail(sp);
+
     assert(sp.shortestSpan() == 3 && "Expect that shortestSpan is 3");
     assert(sp.longestSpan() == 3 && "Expect that longestSpan is 3");
+
     sp.addNumber(17);
     assert(sp.shortestSpan() == 3 && "Expect that shortestSpan is 3");
     assert(sp.longestSpan() == 14 && "Expect that longestSpan is 14");
+
     sp.addNumber(9);
     assert(sp.shortestSpan() == 3 && "Expect that shortestSpan is 3");
     assert(sp.longestSpan() == 14 && "Expect that longestSpan is 14");
+
     sp.addNumber(11);
     assert(sp.shortestSpan() == 2 && "Expect that shortestSpan is 2");
     assert(sp.longestSpan() == 14 && "Expect that longestSpan is 14");
+
     sp.addNumber(12);
     assert(sp.shortestSpan() == 1 && "Expect that shortestSpan is 1");
     assert(sp.longestSpan() == 14 && "Expect that longestSpan is 14");
+
+    if (isPrint == true) {
+    std::cout << "Pass subject test" << '\n';
+    }
 }
 
 int main(int argc, char **argv) {
-    subject_test();
+    if (argc != 2) {
+        std::cerr << "Expect 1 argument got: " << argc << std::endl;
+        return 1;
+    }
+
+    int amount = std::atoi(argv[1]);
+
+    std::vector<int> rv = randomVector(amount);
+    Span x = Span(rv.begin(), rv.end());
+    x.printer();
+
+    unsigned int shortes = x.shortestSpan();
+    unsigned int longest = x.longestSpan();
+
+    std::cout << shortes << std::endl;
+    std::cout << longest << std::endl;
+
+    subject_test(false);
     (void)argc;
     (void)argv;
 }
--- module_08/ex01/tester/src/span_test.cpp
+++ module_08/ex01/tester/src/span_test.cpp
@@ -6,13 +6,16 @@
 /*   By: lvan-gef <lvan-gef@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2024/10/29 18:44:52 by lvan-gef      #+#    #+#                 */
-/*   Updated: 2024/10/29 18:44:55 by lvan-gef      ########   odam.nl         */
+/*   Updated: 2024/11/02 02:27:31 by lvan-gef      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/tester.hpp"
+#include <chrono>
 
 std::vector<int> randomVector(unsigned int N) {
+    auto start = std::chrono::high_resolution_clock::now();
+
     std::random_device rd;
     std::mt19937 gen(rd());
     std::uniform_int_distribution<> dis(std::numeric_limits<int>::min(),
@@ -25,5 +28,10 @@ std::vector<int> randomVector(unsigned int N) {
         tmp.push_back(dis(gen));
     }
 
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration =
+        std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Execution time: " << duration.count() << " milliseconds"
+              << std::endl;
     return tmp;
 }
