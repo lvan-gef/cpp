971d78c693c757a46152f321b913459445beeb65
Luuk Van Geffen <lvan-gef@f0r5s1.codam.nl>
2024-11-11 22:35:57 +0100

    Update tester

--- module_09/ex01/tester/gen_rpn.py
+++ module_09/ex01/tester/gen_rpn.py
@@ -67,3 +67,71 @@ def multi_op_tester(size: int) -> Tuple[str, np.float32]:
 
     result = output.pop()
     return ' '.join((str(x) for x in expression)), result
+
+
+def valid_zero_test() -> Generator[Tuple[str, np.float32], None, None]:
+    for op in OPERATORS[:3]:
+        for neg in range(INT_MIN, 1):
+            yield f'{neg} {0} {op}', _do_op(op=op, lhs=neg, rhs=0)
+            yield f'{0} {neg} {op}', _do_op(op=op, lhs=0, rhs=neg)
+
+            for pos in range(0, INT_MAX):
+                yield f'{pos} {0} {op}', _do_op(op=op, lhs=pos, rhs=0)
+                yield f'{0} {pos} {op}', _do_op(op=op, lhs=0, rhs=pos)
+
+
+def div_zero_test() -> Generator[Tuple[str, bool], None, None]:
+    for nbr in range(INT_MIN, INT_MAX):
+        yield f'{nbr} 0 /', False
+
+    yield '0 0 0 0 0 + - - /', False
+    yield '1 1 1 1 1 + - - /', False
+    yield '2 2 2 2 2 + - - /', False
+    yield '3 3 3 3 3 + - - /', False
+    yield '4 4 4 4 4 + - - /', False
+    yield '5 5 5 5 5 + - - /', False
+    yield '6 6 6 6 6 + - - /', False
+    yield '7 7 7 7 7 + - - /', False
+    yield '8 8 8 8 8 + - - /', False
+    yield '9 9 9 9 9 + - - /', False
+
+
+def decimals_tester(size: int = 25):
+    neg_cycle = itertools.cycle(range(INT_MIN, 0))
+    pos_cycle = itertools.cycle(range(1, INT_MAX))
+
+    for _ in range(size):
+        for neg in neg_cycle:
+            if neg % 2 == 0:
+                continue
+
+            for pos in pos_cycle:
+                if pos % 2 != 0:
+                    continue
+
+                result = _do_op(op='/', lhs=neg, rhs=pos)
+                if not result.is_integer():
+                    yield f'{neg} {pos} /', result
+
+                result = _do_op(op='/', lhs=pos, rhs=neg)
+                if not result.is_integer():
+                    yield f'{pos} {neg} /', result
+                break
+
+            break
+
+
+def really_why_test() -> Tuple[str, np.float32]:
+    inputs = []
+    for x in range(-9, 10):
+        if x == 0:
+            continue
+
+        for y in range(abs(x)):
+            inputs.append(str(x))
+
+    operators = itertools.cycle(OPERATORS)
+    for _ in range(len(inputs) - 1):
+        inputs.append(next(operators))
+
+    return ' '.join(inputs), np.float32(-8.286885245901638)
--- module_09/ex01/tester/rnp_tester.py
+++ module_09/ex01/tester/rnp_tester.py
@@ -2,15 +2,15 @@ import subprocess
 import sys
 from typing import List, Generator, Tuple
 from pathlib import Path
-import itertools
 import numpy as np
 
 import gen_rpn
 
+INT_MIN = -9  # inclusive
+INT_MAX = 10  # exclusive
 
-def get_result(exe: Path, rpn: str) -> List[str]:
-    # print(f'Run {exe} with argument: {rpn}')
 
+def get_result(exe: Path, rpn: str, fail: bool = False) -> List[str]:
     result = subprocess.run(f'{exe} "{rpn}"',
                             shell=True,
                             capture_output=True,
@@ -20,11 +20,20 @@ def get_result(exe: Path, rpn: str) -> List[str]:
     stderr = result.stderr.strip()
 
     try:
-        assert result.returncode == 0
+        if not fail:
+            assert result.returncode == 0
+        else:
+            assert result.returncode != 0
     except AssertionError:
-        print(f'Expect returncode 0 got: {result.returncode}, stderr: {stderr}',
-              file=sys.stderr)
-        exit(1)
+        if not fail:
+            print(f'Expect returncode 0 got: {result.returncode}, stderr: {stderr}',
+                  file=sys.stderr)
+            exit(1)
+        else:
+            print(rpn)
+            print(f'Expect returncode non 0 got: {result.returncode}, stderr: {stderr}',
+                  file=sys.stderr)
+            exit(1)
 
     return [stdout, stderr]
 
@@ -44,62 +53,55 @@ def get_result(exe: Path, rpn: str) -> List[str]:
 
 def valid_inputs(exe: Path) -> Generator[Tuple[str, np.float32], None, None]:
 
-    # Basic operations
+    print('Start basic operations test')
     for expression, result in gen_rpn.basic_op_tester():
         yield expression, result
+    print('Pass basic operations test\n')
 
-    # # Multiple operations
+    # Multiple operations
+    print('Start multiple operations test')
     for x in range(3, 25):
         yield gen_rpn.multi_op_tester(x)
+    print('Pass multiple operations test\n')
 
-    # # Zero handling
-    # yield "0 1 +", 1
-    # yield "1 0 *", 0
-    # yield "0 9 /", 0
-    #
-    # # Negative numbers
-    # yield "-3 -2 +", -5
-    # yield "3 -2 *", -6
-    # yield "-6 3 /", -2
-    #
-    # # More complex expressions
-    # yield "9 2 5 2 * 3 + - *", -99
-    # yield "2 3 4 * 5 - +", 9
-    # yield "3 4 + 5 6 + *", 77
-    #
-    # # Edge cases
-    # for x in range(INT_MIN, INT_MAX):
-    #     yield str(x), x
-    #
-    # # Larger numbers
-    # # "100 50 25 / *", 200
-    # #        100     |    50   | 25  |
-    # yield "5 9 * 2 * 5 5 + + 5 9 * 5 + 5 5 * / *", 200
-    #
-    # # ("999 1 +", 1000),
-    # #           1000           |
-    # yield "5 5 9 9 9 * + + + 5 5 + * 1 1 - +", 1000
-    #
-    # # ("1256 5761 *", 1024),
-    # #           1256       |                   5761              |
-    # yield "6 6 * 6 6 * * 5 8 * - 6 9 * 4 + 5 5 + * 5 5 + * 4 9 * 3 + - *", 7235816
-    #
-    # # Decimal results
-    # yield "5 2 /", 2.5
-    # yield "10 3 /", 3.3333333333333335
-    #
-    # # More complex mathematical expressions
-    # yield "3 4 + 5 6 + * 7 -", 70
-    # yield "2 3 ^ 4 5 ^ +", 1032
-    # yield "10 2 8 * 3 + /", 0.5263157894736842
-    #
-    # # Testing operator precedence
-    # yield "4 5 * 6 7 * +", 62
-    # yield "3 4 5 * + 6 -", 17
-    # yield "2 3 + 4 * 5 2 ^ -", 15
-    #
-    # yield really_why(), 80.08975794403399
+    # Zero handling
+    print('Start valid zero test')
+    for expression, result in gen_rpn.valid_zero_test():
+        yield expression, result
+    print('Pass valid zero test\n')
 
+    print('Start single digit test')
+    for x in range(INT_MIN, INT_MAX):
+        yield str(x), x
+    print('Pass single digit test\n')
+
+    print('Start large numbers test')
+    # "100 50 25 / *", 200
+    yield "5 9 * 2 * 5 5 + + 5 9 * 5 + 5 5 * / *", 200
+    yield "5 8 5 * *", 200
+
+    # "999 1 +", 1000,
+    yield "5 5 9 9 9 * + + + 5 5 + * 1 1 - +", 1000
+    yield "5 5 5 8 * * *", 1000
+
+    # "1256 5761 *", 1024
+    yield "6 6 * 6 6 * * 5 8 * - 6 9 * 4 + 5 5 + * 5 5 + * 4 9 * 3 + - *", 7235816
+    print('Pass large numbers test\n')
+
+    print('Start decimal test')
+    for expression, result in gen_rpn.decimals_tester(size=25):
+        yield expression, result
+    print('Pass decimal test')
+
+    print('Start really why test')
+    yield gen_rpn.really_why_test()
+    print('Pass really why test')
+
+
+def invalid_inputs(exe: Path):
+    print('Start div by zero')
+    for expression, result in gen_rpn.div_zero_test():
+        yield expression, result
     # # Edge cases that should raise errors
     # error_cases = [
     #     "",                # Empty expression
@@ -113,51 +115,18 @@ def valid_inputs(exe: Path) -> Generator[Tuple[str, np.float32], None, None]:
     #     "1.1.1 2 +",      # Malformed number
     #     "2 2 + +",        # Too many operators
     # ]
-    #
-    # for expression, expected in test_cases:
-    #     result = get_result(exe=exe, rpn=expression)
-    #     if result[0] == '':
-    #         print('waarom', result[1], expression)
-    #         continue
-    #
-    #     expected = np.float32(str(expected))
-    #     result[0] = np.float32(result[0])
-    #     try:
-    #         np.testing.assert_almost_equal(
-    #             np.float32(expected),
-    #             np.float32(result[0]),
-    #             decimal=2)
-    #     except AssertionError:
-    #         print(f"Failed: {expression} = {float(result[0])}, expected {expected}")
-    #         exit(1)
-
-
-def really_why():
-    inputs = []
-    for x in range(-9, 10):
-        if x == 0:
-            continue
-
-        for y in range(abs(x)):
-            inputs.append(str(x))
-
-    operators = itertools.cycle(['*', '-', '/', '+'])
-    for _ in range(len(inputs) - 1):
-        inputs.append(next(operators))
-
-    return ' '.join(inputs)
 
 
 def run_tests(exe: Path):
-    print('Start valid inputs test')
+    print('Start valid inputs test\n')
     for expression, expected in valid_inputs(exe=exe):
         result, err = get_result(exe=exe, rpn=expression)
 
         try:
             assert not err
         except AssertionError:
-            print(f'Except that there are no errors, got: \'{
-                  err}\'', file=sys.stderr)
+            print(f'{expression}')
+            print(f'Except that there are no errors, got: \'{err}\'', file=sys.stderr)
             exit(1)
 
         try:
@@ -166,11 +135,25 @@ def run_tests(exe: Path):
                 expected, result, decimal=2)
         except AssertionError:
             print(
-                f'on input: \'{expression}\', we expect: \'{
-                    expected:.2f}\', got: \'{result:.2f}\'',
+                f'on input: \'{expression}\', we expect: \'{expected:.2f}\', got: \'{result:.2f}\'',
                 file=sys.stderr)
             exit(1)
-    print('Pass valid inputs test')
+    print('\nPass valid inputs test\n')
+
+    print('Start invalid test\n')
+    for expression, _ in invalid_inputs(exe=exe):
+        result, err = get_result(exe=exe, rpn=expression, fail=True)
+        try:
+            assert not result
+        except AssertionError:
+            print(f'Except that there are no messages on stdout, got: \'{result}\'', file=sys.stderr)
+            exit(1)
+
+        try:
+            assert err
+        except AssertionError:
+            print(f'Except that there is a errors, got: \'{err}\'', file=sys.stderr)
+            exit(1)
 
 
 if __name__ == '__main__':
