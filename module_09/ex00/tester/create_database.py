23757e6d7cc88617cf993490058decf072b20382
luuk <lvan-gef@student.codam.nl>
2024-11-13 05:19:13 +0100

    If date not in db add it with prev date's value

--- module_09/ex00/tester/create_database.py
+++ module_09/ex00/tester/create_database.py
@@ -6,14 +6,11 @@ import datetime
 
 import numpy as np
 
-
-# first value of db can not be more then 1000 i have no idea what to then...
 def load_test_db2(path: Path, delim: str = ',') -> dict[datetime.date, np.float32]:
     """
-    Load in the csv file, if we have missing date's we will add those.
-    The value of it is the previous date in db.
+    Load in the csv file, if we have missing dates we will add those.
+    For any missing date, use the last available value from the database.
     """
-
     db = {}
 
     with open(path, newline='') as csvfile:
@@ -24,33 +21,23 @@ def load_test_db2(path: Path, delim: str = ',') -> dict[datetime.date, np.float3
             value = np.float32(float(row[1]))
             db[date] = value
 
-    # Early return if empty
     if not db:
         return {}
 
-    # Get date range
     dates = sorted(db.keys())
     start_date = dates[0]
     end_date = dates[-1]
 
-    # Initialize result dictionary with all dates
     result = {}
     current_date = start_date
+    last_value = db[start_date]
 
-    # Fill all dates between start and end
     while current_date <= end_date:
         if current_date in db:
-            # If current value is > 1000 and not the first entry,
-            # use the previous day's value
-            if db[current_date] > np.float32(1000) and current_date != start_date:
-                result[current_date] = result[current_date -
-                                              datetime.timedelta(days=1)]
-            else:
-                result[current_date] = db[current_date]
+            result[current_date] = db[current_date]
+            last_value = db[current_date]
         else:
-            # For missing dates, use the previous day's value
-            result[current_date] = result[current_date -
-                                          datetime.timedelta(days=1)]
+            result[current_date] = last_value
 
         current_date += datetime.timedelta(days=1)
 
