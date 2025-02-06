27fafe51bfe74d693916b44705f6c4e861cc6b49
luuk <lvan-gef@student.codam.nl>
2025-02-06 03:00:31 +0100

    Create new testing

---
+++ module_09/ex02/tester/include/PmergeMeTester.hpp
@@ -0,0 +1,6 @@
+#ifndef PMERGMETESTER_HPP
+#define PMERGMETESTER_HPP
+
+int runTest(int argc, char **argv);
+
+#endif // !PMERGMETESTER_HPP
---
+++ module_09/ex02/tester/src/PmergeMeTester.cpp
@@ -0,0 +1,22 @@
+#include <cerrno>
+#include <cstdio>
+#include <unistd.h>
+#include <vector>
+
+#include "../../include/PmergeVector.hpp"
+#include "../include/PmergeMeTester.hpp"
+
+int runTest(int argc, char **args) {
+    PmergeVector vec;
+
+    std::vector<int> vec_result = vec.sort(argc, args);
+    if (errno != 0) {
+        return 1;
+    }
+    vec.printSeq(vec_result, argc - 1);
+
+    return 0;}
+
+int main(int argc, char **argv) {
+    return runTest(argc, argv);
+}
