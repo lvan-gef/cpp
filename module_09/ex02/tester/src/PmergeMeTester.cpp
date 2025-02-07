a6123eb4874511a885533b4613c91fd9985d0d24
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-02-07 14:51:03 +0100

    Update the wrapper for the tester

--- module_09/ex02/tester/src/PmergeMeTester.cpp
+++ module_09/ex02/tester/src/PmergeMeTester.cpp
@@ -1,5 +1,5 @@
 #include <cerrno>
-#include <cstdio>
+#include <iostream>
 #include <unistd.h>
 #include <vector>
 
@@ -10,14 +10,17 @@ int runTest(int argc, char **args) {
     PmergeVector vec;
 
     std::vector<int> vec_result = vec.sort(argc, args);
-    if (errno != 0) {
+    if (errno != 0 || vec_result.empty()) {
         return 1;
     }
-    vec.printSeq(vec_result, argc - 1);
 
+    vec.printSeq(vec_result, argc - 1);
     return 0;
 }
 
 int main(int argc, char **argv) {
-    return runTest(argc, argv);
+    int result = runTest(argc, argv);
+    std::cerr.flush();
+    std::cout.flush();
+    return result;
 }
